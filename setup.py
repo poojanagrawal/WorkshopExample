d2d6707ad0702314e05ef24efc6df078091437f0
poojanagrawal <pagrawal7@gmail.com>
2017-12-05 13:47:54 +1100

    trial1

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
-      url='http://github.com/Samreay/WorkshopExample',
+      url='http://github.com/poojanagrawal/WorkshopExample',
       author='Samuel Hinton',
-      author_email='samuelreay@gmail.com',
+      author_email='poojanagrawal7@gmail.com',
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages(exclude=('tests', 'doc', 'data'))
